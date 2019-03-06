ad8394829ed1317d5295e322ac767d611e462300
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-06 18:47:17 -0500

    action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,28 @@
 // we'll need axios
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR  = 'ERROR';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
-
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
+// the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import  combineReducers  from './reducers';
-
-
-const store = createStore( combineReducers, applyMiddleware(thunk, logger))
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export const fetch = () => { 
+    return dispatch => {
+        dispatch({type: FETCHING});
+        axios
+        .get('https://swapi.co/api/people/')
+        .then(response => {
+            console.log(response.data.results);
+            dispatch({type: FETCHED, chars: response.data.results});
+        })
+        .catch(response => {
+            dispatch({type: ERROR, error: response.message});
+        });
+    }
+}
